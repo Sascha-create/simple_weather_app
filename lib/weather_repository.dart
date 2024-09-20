4af3ae8aa91866c10ef00019be9cf3be10ee750e
Sascha Ziegler <sascha8398@googlemail.com>
2024-09-20 18:42:22 +0200

    added random Function to weather repository

--- lib/weather_repository.dart
+++ lib/weather_repository.dart
@@ -1,8 +1,17 @@
+import 'dart:math';
+
 import 'package:simple_weather_app/weather_data.dart';
 
-class WeatherRepository {
-  final WeatherData _weatherData = WeatherData(
-      city: "Karlsruhe", temperature: 25, weatherCondition: "sonnig");
+List<WeatherData> weatherDatas = [
+  WeatherData(city: "Karlsruhe", temperature: 25, weatherCondition: "sonnig"),
+  WeatherData(city: "Landau", temperature: 20, weatherCondition: "regnerisch"),
+  WeatherData(city: "Offenbach", temperature: 22, weatherCondition: "wolkig"),
+  WeatherData(city: "Lohr", temperature: 17, weatherCondition: "heiter"),
+];
 
-  WeatherData getWeather() => _weatherData;
+class WeatherRepository {
+  final WeatherData _weatherData =
+      weatherDatas[Random().nextInt(weatherDatas.length)];
+
+  WeatherData getRandomWeather() => _weatherData;
 }
