862969228b57093a9964698afe2e179ee0bdd00c
Sascha Ziegler <sascha8398@googlemail.com>
2024-09-20 14:18:15 +0200

    modified Appbar and added Welcometext

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,40 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-   const MainApp({super.key}); 
+  const MainApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WeatherApp(),
+    );
+  }
+}
 
-   @override
-   Widget build(BuildContext context) {
-     return const MaterialApp(
-       home: WeatherApp(),
-     );
-   }
- }
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
 
-
- class WeatherApp extends StatelessWidget {
-   const WeatherApp({super.key});
-
-
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
+        elevation: 2,
+        shadowColor: Colors.grey,
+        centerTitle: true,
         title: const Text("Wetter App"),
       ),
-     );
-   }
- }
-
+      body: const SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                "Willkommen zur Wetter-App!",
+                style: TextStyle(fontSize: 24, color: Colors.blue),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
